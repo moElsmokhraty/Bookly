5b8c87c62bbf4be0147f64d289f3b721624da54a
moElsmokhraty <melsmokhraty@yahoo.com>
2023-08-06 05:54:13 +0300

    Create Remote Data Source

---
+++ lib/core/functions/get_books_list.dart
@@ -0,0 +1,10 @@
+import 'package:bookly/core/models/book_model/book_model.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+
+List<BookEntity> getBooksList(Map<String, dynamic> data) {
+  List<BookEntity> books = [];
+  for (var item in data['items']) {
+    books.add(BookModel.fromJson(item));
+  }
+  return books;
+}
